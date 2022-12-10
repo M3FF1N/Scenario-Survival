292178b54d005fb9fb9a1b5da2417233781fcc86
Joshua <99350806+M3FF1N@users.noreply.github.com>
2022-12-10 22:05:20 +0000

    Add ActionResult.h

---
+++ ActionResult.h
@@ -0,0 +1,14 @@
+#include <string>
+using namespace std;
+class ActionResult {
+private:
+
+public:
+ActionResult(); // constructor
+
+string resultString;
+int resultHealth;
+int resultStamina;
+
+};
+
